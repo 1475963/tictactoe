0f5af1961138b2e8ae6b0c5a9bd685ab94599f9f
1475963 <benzahramarc@yahoo.fr>
2015-08-30 02:47:15 +0200

    change readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# tictactoe
-csb
+Ugly tictactoe game made with tkinter
+
+tkinter is implemented in python 3.x+ standard library
+
+In order to start this you must install python 3.x+ (tested on 3.4.3)
+
+### Usage
+
+* Linux - "./tictactoe"
+* Windows (yay cmd/cmder/cygwin) - "python tictactoe" (see Linux in case of cygwin usage :3)
--- consts.py
+++ consts.py
@@ -4,23 +4,23 @@
 
 ''' main consts '''
 
-USAGE	= "USAGE:\t./tictactoe [nothanks]"
+USAGE   = "USAGE:\t./tictactoe [nothanks]"
 
 ''' board consts '''
 
-BOA_HEIGHT			= 3
-BOA_WIDTH			= 3
-BOA_FIELD_TYPECODE	= 'B'
-BOA_EMPTY_REPR		= 0
-BOA_P1_REPR			= 1
-BOA_P2_REPR			= 2
-BOA_VALID_REPR		= [BOA_EMPTY_REPR,
-					   BOA_P1_REPR,
-					   BOA_P2_REPR]
+BOA_HEIGHT          = 3
+BOA_WIDTH           = 3
+BOA_FIELD_TYPECODE  = 'B'
+BOA_EMPTY_REPR      = 0
+BOA_P1_REPR         = 1
+BOA_P2_REPR         = 2
+BOA_VALID_REPR      = [BOA_EMPTY_REPR,
+                       BOA_P1_REPR,
+                       BOA_P2_REPR]
 
 ''' tkinter consts '''
 
-TK_BG_COLOR 		= '#ffffff'
-TK_BG_STICKS_COLOR	= '#000000'
-TK_COLOR_BLACK		= '#000000'
-TK_UPDATE_TIMER		= 500
+TK_BG_COLOR         = '#ffffff'
+TK_BG_STICKS_COLOR  = '#000000'
+TK_COLOR_BLACK      = '#000000'
+TK_UPDATE_TIMER     = 250
