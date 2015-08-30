304b675115cdde44b7d056e707965d1e26cf637c
1475963 <benzahramarc@yahoo.fr>
2015-08-30 02:29:42 +0200

    we need a referee boys, win condition is aweful :< ; next: refactor code (sadness), referee (yay), add menu, retry, end screen and IA, some gui improvements pls (this gui doesn't give any boner)

--- Board.py
+++ Board.py
@@ -4,14 +4,15 @@ Board class using bit field array in order to store game data
 
 import consts as Consts
 import array
+from random import sample
 
-# temporary tests waiting for a event manager
-from random import randint
+DEBUG = False
 
 
 class Board(object):
 
     def __init__(self):
+        self.turn = sample(Consts.BOA_VALID_REPR[1:], 1)[0]
         self.field = array.array(Consts.BOA_FIELD_TYPECODE)
         self.height = Consts.BOA_HEIGHT
         self.width = Consts.BOA_HEIGHT
@@ -23,24 +24,67 @@ class Board(object):
     def __ResetBoard(self):
         for y in range(self.height):
             for x in range(self.width):
-                self.field.append(0)
-
-        # temporary tests waiting for a event manager
-        self.setFrame(randint(0, 2), randint(0, 2), Consts.BOA_P1_REPR)
-        self.setFrame(randint(0, 2), randint(0, 2), Consts.BOA_P1_REPR)
-        self.setFrame(randint(0, 2), randint(0, 2), Consts.BOA_P1_REPR)
-        self.setFrame(randint(0, 2), randint(0, 2), Consts.BOA_P1_REPR)
+                self.field.append(Consts.BOA_EMPTY_REPR)
 
     def getFrame(self, pX, pY):
         return self.field[self.__TransformCoords(pX, pY)]
 
-    def setFrame(self, pX, pY, pValue):
+    def setFrame(self, pX, pY):
         localFrame = self.field[self.__TransformCoords(pX, pY)]
         if localFrame == Consts.BOA_EMPTY_REPR:
-            self.field[self.__TransformCoords(pX, pY)] = pValue
+            self.field[self.__TransformCoords(pX, pY)] = self.turn
+            if self.checkWinnance(pX, pY):
+                winner = 'VOID LOL'
+                if self.turn == Consts.BOA_P1_REPR:
+                    winner = 'P1 - CROSS'
+                elif self.turn == Consts.BOA_P2_REPR:
+                    winner = 'P2 - CIRCLE'
+                raise Exception('ALLER CHAMPION (%s) A GAGNER' % winner)
+            self.fireNextTurn()
+
+    def checkWinnance(self, pX, pY):
+        def adjustX(self, pX):
+            return pX % self.width
+
+        def adjustY(self, pY):
+            return pY % self.height
+
+        if ((self.getFrame(pX, pY) == self.getFrame(adjustX(self, pX + 1), pY)
+                 and self.getFrame(pX, pY) == self.getFrame(adjustX(self, pX - 1), pY))
+            or (self.getFrame(pX, pY) == self.getFrame(pX, adjustY(self, pY + 1))
+                and self.getFrame(pX, pY) == self.getFrame(pX, adjustY(self, pY - 1)))
+            or (self.getFrame(pX, pY) == self.getFrame(adjustX(self, pX + 1), adjustY(self, pY + 1))
+                and self.getFrame(pX, pY) == self.getFrame(adjustX(self, pX - 1), adjustY(self, pY - 1)))
+            or (self.getFrame(pX, pY) == self.getFrame(adjustX(self, pX + 1), adjustY(self, pY - 1))
+                and self.getFrame(pX, pY) == self.getFrame(adjustX(self, pX - 1), adjustY(self, pY + 1)))):
+            return True
+        if not any(True if elem == Consts.BOA_EMPTY_REPR else False for elem in self.field):
+            raise Exception('TIE BREAKER BOYS')
+        return False
+
+    def fireNextTurn(self):
+        players = Consts.BOA_VALID_REPR[1:]
+        try:
+            if DEBUG:
+                print('players : ', players)
+                print('turn : ', self.turn)
+            currentFoundAt = players.index(self.turn)
+            if DEBUG:
+                print('current : ', currentFoundAt)
+            self.turn = players[(currentFoundAt + 1) % len(players)]
+        except Exception:
+            print('currentPlayerNotFound !!!')
 
     def __TransformCoords(self, pX, pY):
         return (pY * self.width) + pX
+
+    @property
+    def turn(self):
+        return self.__turn
+
+    @turn.setter
+    def turn(self, pValue):
+        self.__turn = pValue
 
     @property
     def field(self):
--- tk_draw.py
+++ tk_draw.py
@@ -4,7 +4,7 @@ from tkinter import *
 import board_properties as Config
 import consts as Consts
 
-DEBUG = True
+DEBUG = False
 
 
 def get_instance():
@@ -12,17 +12,46 @@ def get_instance():
     return Tk()
 
 
-def get_window(pInstance):
+def clickHandler(event, pBoard):
+    if DEBUG:
+        print('click at x : (%d), y : (%d)' % (event.x, event.y))
+    scaledPoint = scaleGraphicToBoard((event.x, event.y))
+    if DEBUG:
+        print('stupid scale, x : (%d), y : (%d)' % (scaledPoint[0],
+                                                    scaledPoint[1]))
+    pBoard.setFrame(scaledPoint[0], scaledPoint[1])
+
+
+def keyboardHandler(event):
+#    if DEBUG:
+    print('keystroke pressed : ', repr(event.char))
+
+
+def updateHandler(pInstance, pWindow, pBoard):
+    if DEBUG:
+        print('update')
+    draw_board(pWindow, pBoard)
+    attachUpdater(pInstance, pWindow, pBoard)
+
+
+def get_window(pInstance, pBoard):
     ''' Returns a canvas object to draw on '''
     canvas = Canvas(pInstance, width=Config.WIDTH, height=Config.HEIGHT)
+    canvas.bind('<Key>', keyboardHandler)
+    canvas.bind('<ButtonPress-1>',
+                lambda event, board=pBoard: clickHandler(event, board))
     canvas.pack()
     return canvas
 
 
-def do_mainloop():
+def attachUpdater(pInstance, pWindow, pBoard):
+    pInstance.after(Consts.TK_UPDATE_TIMER, updateHandler,
+                    pInstance, pWindow, pBoard)
+
+
+def attachMainloop(pInstance):
     ''' Calls Tkinter mainloop() function '''
-    mainloop()
-    return
+    pInstance.mainloop()
 
 
 def scaleBoardToGraphic(pPoint):
@@ -32,6 +61,16 @@ def scaleBoardToGraphic(pPoint):
                 raise Exception('bad point format')
         return ((pPoint[0] * Config.WIDTH) / 3,
                 (pPoint[1] * Config.HEIGHT) / 3)
+
+
+def scaleGraphicToBoard(pPoint):
+    if len(pPoint) == 2:
+        for i in range(2):
+            if (not isinstance(pPoint[i], int)
+                    and not isinstance(pPoint[i], float)):
+                raise Exception('bad point format')
+        return (int((pPoint[0] / Config.WIDTH) * 3),
+                int((pPoint[1] / Config.HEIGHT * 3)))
 
 
 def draw_cross(pWindow, pX, pY):
@@ -48,6 +87,20 @@ def draw_cross(pWindow, pX, pY):
         print('SECOND LINE:: start point : {}, end point : {}'.format(sPoint,
                                                                       ePoint))
     pWindow.create_line(sPoint[0], sPoint[1], ePoint[0], ePoint[1])
+
+def draw_circle(pWindow, pX, pY):
+    sPoint = scaleBoardToGraphic((pX, pY))
+    ePoint = scaleBoardToGraphic((pX + 1, pY + 1))
+    if DEBUG:
+        print('CIRCLE:: start point : {}, end point : {}'.format(sPoint,
+                                                                 ePoint))
+    pWindow.create_oval(sPoint[0],
+                        sPoint[1],
+                        ePoint[0],
+                        ePoint[1],
+                        outline=Consts.TK_COLOR_BLACK,
+                        fill=Consts.TK_BG_COLOR,
+                        width=2)
 
 def draw_board(pWindow, pBoard):
     '''
@@ -78,9 +131,6 @@ def draw_board(pWindow, pBoard):
             if frame in Consts.BOA_VALID_REPR:
                 if frame == Consts.BOA_P1_REPR:
                     draw_cross(pWindow, x, y)
-                    # call function to draw a cross
-                    pass
                 elif frame == Consts.BOA_P2_REPR:
-                    # call function to draw a circle
-                    pass
+                    draw_circle(pWindow, x, y)
     return
